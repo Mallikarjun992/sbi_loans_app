016bda1472531b8da50ef751666a3d4667a1832f
Mallikarjun992 <arjun3490@gmail.com>
2025-04-25 15:00:22 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,12 @@
 class Test
 {
+	public static void test()
+	{
+		System.out.println("test method added");
+	}
 	public static void main(String ... args)
 	{
+		Test.test();
 		System.out.println("welcome to pune...");
 	}
 }
