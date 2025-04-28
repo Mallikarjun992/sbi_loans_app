12ef44cd7e479cff07772eb7e4932772663e770d
Mallikarjun992 <arjun3490@gmail.com>
2025-04-28 12:32:57 +0530

    comming the changes

--- demo.java
+++ demo.java
@@ -3,5 +3,6 @@ class demo
 	public static void main(String ... agrs)
 	{
 		System.out.println("welcome to bangalore");
+		System.out.prinltn("trying to revert my changes from central to local");
 	}
 }
