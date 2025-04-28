fe3c18af45b185577628f97603e2699f318ee4ca
Mallikarjun992 <arjun3490@gmail.com>
2025-04-28 12:35:39 +0530

    reverting my changes

--- demo.java
+++ demo.java
@@ -3,6 +3,5 @@ class demo
 	public static void main(String ... agrs)
 	{
 		System.out.println("welcome to bangalore");
-		System.out.prinltn("trying to revert my changes from central to local");
 	}
 }
